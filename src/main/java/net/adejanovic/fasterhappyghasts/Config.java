2bcfa4380e7a9e293492b3566dc0d8827f58d41b
Aleksa Dejanović <dejanovicaleksa234@gmail.com>
2025-10-31 20:12:10 +0100

    initial version with config

---
+++ src/main/java/net/adejanovic/fasterhappyghasts/Config.java
@@ -0,0 +1,36 @@
+package net.adejanovic.fasterhappyghasts;
+
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.Item;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.fml.event.config.ModConfigEvent;
+import net.neoforged.neoforge.common.ModConfigSpec;
+
+// An example config class. This is not required, but it's a good idea to have one to keep your config organized.
+// Demonstrates how to use Neo's config APIs
+public class Config {
+    public static final ModConfigSpec SPEC;
+    public static final ModConfigSpec.DoubleValue GHAST_MOUNT_SPEED;
+    public static final ModConfigSpec.DoubleValue GHAST_DEFAULT_SPEED;
+
+    static {
+        ModConfigSpec.Builder builder = new ModConfigSpec.Builder();
+
+        builder.push("ghast");
+        GHAST_MOUNT_SPEED = builder
+                .comment("Flying speed when mounted")
+                .defineInRange("mount_speed", 0.7, 0.0, 1.0);
+        GHAST_DEFAULT_SPEED = builder
+                .comment("Flying speed when not mounted")
+                .defineInRange("default_speed", 0.05, 0.0, 1.0);
+        builder.pop();
+
+        SPEC = builder.build();
+    }
+}
---
+++ src/main/java/net/adejanovic/fasterhappyghasts/FasterHappyGhasts.java
@@ -0,0 +1,78 @@
+package net.adejanovic.fasterhappyghasts;
+
+import net.minecraft.world.entity.ai.attributes.Attributes;
+import net.minecraft.world.entity.animal.HappyGhast;
+import net.minecraft.world.entity.monster.Ghast;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.*;
+import net.neoforged.neoforge.event.entity.EntityMountEvent;
+import org.slf4j.Logger;
+
+import com.mojang.logging.LogUtils;
+
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.food.FoodProperties;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.material.MapColor;
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.config.ModConfig;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.neoforged.neoforge.common.NeoForge;
+import net.neoforged.neoforge.event.BuildCreativeModeTabContentsEvent;
+import net.neoforged.neoforge.event.server.ServerStartingEvent;
+import net.neoforged.neoforge.registries.DeferredBlock;
+import net.neoforged.neoforge.registries.DeferredHolder;
+import net.neoforged.neoforge.registries.DeferredItem;
+import net.neoforged.neoforge.registries.DeferredRegister;
+
+import java.util.Objects;
+
+// The value here should match an entry in the META-INF/neoforge.mods.toml file
+@Mod(FasterHappyGhasts.MODID)
+public class FasterHappyGhasts {
+    // Define mod id in a common place for everything to reference
+    public static final String MODID = "fasterhappyghasts";
+    // Directly reference a slf4j logger
+    public static final Logger LOGGER = LogUtils.getLogger();
+
+
+    // The constructor for the mod class is the first code that is run when your mod is loaded.
+    // FML will recognize some parameter types like IEventBus or ModContainer and pass them in automatically.
+    public FasterHappyGhasts(IEventBus modEventBus, ModContainer modContainer) {
+
+        // Register ourselves for server and other game events we are interested in.
+        // Note that this is necessary if and only if we want *this* class (FasterHappyGhasts) to respond directly to events.
+        // Do not add this line if there are no @SubscribeEvent-annotated functions in this class, like onServerStarting() below.
+        NeoForge.EVENT_BUS.register(this);
+
+        // Register the item to a creative tab
+
+        // Register our mod's ModConfigSpec so that FML can create and load the config file for us
+        modContainer.registerConfig(ModConfig.Type.COMMON, Config.SPEC);
+    }
+
+    @SubscribeEvent
+    public void onMount(EntityMountEvent event) {
+        if (event.isMounting()
+                && event.getEntityMounting() instanceof Player player
+                && event.getEntityBeingMounted() instanceof HappyGhast ghast) {
+            Objects.requireNonNull(ghast.getAttribute(Attributes.FLYING_SPEED)).setBaseValue(Config.GHAST_MOUNT_SPEED.get());
+        }
+    }
+
+    @SubscribeEvent
+    public void onDismount(EntityMountEvent event) {
+        if (event.isDismounting()
+                && event.getEntityBeingMounted() instanceof HappyGhast ghast) {
+            Objects.requireNonNull(ghast.getAttribute(Attributes.FLYING_SPEED)).setBaseValue(Config.GHAST_DEFAULT_SPEED.get());
+        }
+    }
+}
