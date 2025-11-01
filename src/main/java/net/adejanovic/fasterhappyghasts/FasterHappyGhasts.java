4dd8206c26dabee47e63b3ea08bdb4890f1e7b95
Aleksa Dejanović <dejanovicaleksa234@gmail.com>
2025-11-01 15:19:45 +0100

    cleanup

--- src/main/java/net/adejanovic/fasterhappyghasts/FasterHappyGhasts.java
+++ src/main/java/net/adejanovic/fasterhappyghasts/FasterHappyGhasts.java
@@ -41,7 +41,7 @@ public class FasterHappyGhasts {
     // Define mod id in a common place for everything to reference
     public static final String MODID = "fasterhappyghasts";
     // Directly reference a slf4j logger
-    public static final Logger LOGGER = LogUtils.getLogger();
+    // public static final Logger LOGGER = LogUtils.getLogger();
 
 
     // The constructor for the mod class is the first code that is run when your mod is loaded.
@@ -49,11 +49,7 @@ public class FasterHappyGhasts {
     public FasterHappyGhasts(IEventBus modEventBus, ModContainer modContainer) {
 
         // Register ourselves for server and other game events we are interested in.
-        // Note that this is necessary if and only if we want *this* class (FasterHappyGhasts) to respond directly to events.
-        // Do not add this line if there are no @SubscribeEvent-annotated functions in this class, like onServerStarting() below.
         NeoForge.EVENT_BUS.register(this);
-
-        // Register the item to a creative tab
 
         // Register our mod's ModConfigSpec so that FML can create and load the config file for us
         modContainer.registerConfig(ModConfig.Type.COMMON, Config.SPEC);
